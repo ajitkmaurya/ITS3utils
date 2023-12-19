96053602402fa72aa77ced63b2ec7d3d3c5500ac
Ajit Kumar <ajit.310465@gmail.com>
2023-12-19 12:18:07 +0100

    update

--- qa/run_noise.sh
+++ qa/run_noise.sh
@@ -4,11 +4,5 @@ pcb_no=$1
 hv=$2
 run=$3
 
-/home/akumar/ce65v2_ana_soft/ITS3utils/eudaq/CE65V2Dump.py /home/akumar/ce65v2_ana_soft/data/desy/noise/ce65v2_pcb${pcb_no}_hv${hv}_noise_run${run}.raw -o pcb${pcb_no}-hv${hv}-noise -e 5000 -n -d --qa
-/home/akumar/ce65v2_ana_soft/ITS3utils/eudaq/analog_qa_ce54v2.py pcb${pcb_no}-hv${hv}-noise-qa.root -o pcb${pcb_no}_hv${hv}-noisemap --rts --frac 0.9
-
-#../../../ITS3utils/eudaq/CE65V2Dump.py ../../../data/desy/noise/ce65v2_pcb03_hv10_noise_run483174912_231129175816.raw -o pcb03-hv10-noise -e 5000 -n -d --qa
-#../../../ITS3utils/eudaq/analog_qa_ce54v2.py pcb03-hv10-noise-qa.root -o pcb03_hv10-noisemap --rts --frac 0.9
-
-#../../../ITS3utils/eudaq/CE65V2Dump.py ../../../data/desy/noise/ce65v2_pcb18_hv10_noise_run484085812_231130090903.raw -o pcb18-hv10-noise -e 5000 -n -d --qa
-#../../../ITS3utils/eudaq/analog_qa_ce54v2.py pcb18-hv10-noise-qa.root -o pcb18_hv10-noisemap --rts --frac 0.9
+../eudaq/CE65V2Dump.py ../data/ce65v2_pcb${pcb_no}_hv${hv}_noise_run${run}.raw -o pcb${pcb_no}-hv${hv}-noise -e 5000 -n -d --qa
+../eudaq/analog_qa_ce65v2.py pcb${pcb_no}-hv${hv}-noise-qa.root -o pcb${pcb_no}_hv${hv}-noisemap --rts --frac 0.9
